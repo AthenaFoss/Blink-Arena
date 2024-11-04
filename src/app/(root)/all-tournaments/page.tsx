0c80020b43827ae97dc6a6bfad6acd811ffcebdb
AvhiMaz <avhimazumder5@outlook.com>
2024-11-04 07:55:18 +0530

    delete button in all tournaments page

--- src/app/(root)/all-tournaments/page.tsx
+++ src/app/(root)/all-tournaments/page.tsx
@@ -9,6 +9,7 @@ import Image from "next/image";
 import Button from "@/components/ui/arrow-right";
 import ButtonLeft from "@/components/ui/arrow-left";
 import Link from "next/link";
+import { Trash } from "lucide-react";
 
 interface Tournament {
   _id: string;
@@ -118,63 +119,70 @@ const Page = () => {
   };
 
   return (
-    <div
-      className="flex justify-center items-center min-h-screen relative"
-      style={{
-        background:
-          "radial-gradient(ellipse at center, rgba(0,0,0,0) 0%, rgba(0,0,0,0.7) 100%)",
-      }}
-    >
-      <div className="absolute top-0 left-0 w-full h-full z-10 bg-gradient-to-b from-stone-900 to-purple-900">
-        <Image
-          src="/blink-img-1.png"
-          layout="fill"
-          alt="alt"
-          className="object-cover w-full h-full blur-md"
-        />
-        <div
-          className="absolute top-0 left-0 w-full h-full pointer-events-none z-20"
-          style={{
-            backgroundImage: "url('/noise.png')",
-            opacity: 0.8,
-            mixBlendMode: "overlay",
-          }}
-        />
-      </div>
-      <Link href="/">
-        <div className="absolute top-12 left-12 z-50 max-w-xl">
-          <div className="text-left">
+    <>
+      <div
+        className="flex justify-center items-center min-h-screen relative"
+        style={{
+          background:
+            "radial-gradient(ellipse at center, rgba(0,0,0,0) 0%, rgba(0,0,0,0.7) 100%)",
+        }}
+      >
+        <div className="absolute top-0 left-0 w-full h-full z-10 bg-gradient-to-b from-stone-900 to-purple-900">
+          <Image
+            src="/blink-img-1.png"
+            layout="fill"
+            alt="alt"
+            className="object-cover w-full h-full blur-md"
+          />
+          <div
+            className="absolute top-0 left-0 w-full h-full pointer-events-none z-20"
+            style={{
+              backgroundImage: "url('/noise.png')",
+              opacity: 0.8,
+              mixBlendMode: "overlay",
+            }}
+          />
+        </div>
+
+        <div className="absolute top-12 left-12 z-50 flex items-center justify-end gap-4">
+          <Link href="/">
             <h1 className="text-white text-2xl font-medium">
               blink{" "}
-              <span className="px-2 py-1 bg-gradient-to-r from-purple-600 via-fuchsia-700 to-purple-900  rounded-md text-slate-200">
+              <span className="px-2 py-1 bg-gradient-to-r from-purple-600 via-fuchsia-700 to-purple-900 rounded-md text-slate-200">
                 arena
               </span>
             </h1>
-          </div>
+          </Link>
+          <Link href="/delete">
+            <button className="bg-red-600 text-white py-2 px-4 rounded-md hover:bg-red-700">
+              <Trash />
+            </button>
+          </Link>
         </div>
-      </Link>
-      <div className="relative z-30 w-full max-w-[100rem]">
-        <Slider {...settings}>
-          {data.map((tournament) => (
-            <div key={tournament._id} className="px-[250px]">
-              <TournamentCard
-                tournamentId={tournament.tournamentId}
-                title={tournament.organizationName}
-                description={tournament.description}
-                totalSlot={tournament.totalSlot}
-                publicKey={tournament.publicKey}
-                image={tournament.image}
-                date={tournament.date}
-                time={tournament.time}
-                location={tournament.location}
-                prizePool={tournament.prizePool}
-                joinFees={tournament.joinFees}
-              />
-            </div>
-          ))}
-        </Slider>
+
+        <div className="relative z-30 w-full max-w-[100rem]">
+          <Slider {...settings}>
+            {data.map((tournament) => (
+              <div key={tournament._id} className="px-[250px]">
+                <TournamentCard
+                  tournamentId={tournament.tournamentId}
+                  title={tournament.organizationName}
+                  description={tournament.description}
+                  totalSlot={tournament.totalSlot}
+                  publicKey={tournament.publicKey}
+                  image={tournament.image}
+                  date={tournament.date}
+                  time={tournament.time}
+                  location={tournament.location}
+                  prizePool={tournament.prizePool}
+                  joinFees={tournament.joinFees}
+                />
+              </div>
+            ))}
+          </Slider>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
